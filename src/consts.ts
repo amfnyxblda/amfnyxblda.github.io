e6f8875e2a50f0bf96b4635a8403f203d2c40b46
digital <digital@jagapet.com>
2026-04-29 10:21:19 +0800

    changed menu languages

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place this file in `src/consts.ts`
-export const SITE_TITLE = 'Jagapet';
-export const SITE_DESCRIPTION = 'A collection of pet care resources';
-export const GITHUB_URL = 'https://github.com/lingolist';
-export const CATEGORY_POSTS_PER_PAGE = 4;
